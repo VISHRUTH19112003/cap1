218e7a56e0981b280fdd9eeb2b5f5736f01213d2
Vishruth H V <vishruthhvasudevarao@gmail.com>
2025-11-14 11:33:38 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-4588793295-cb701",
-  "appId": "1:247681701203:web:e7f6c6257065cf5f94fda6",
+  "projectId": "nyayagpt-426415",
+  "appId": "1:247681701203:web:e41d8c117e30217c94fda6",
   "apiKey": "AIzaSyAzGVK9rd_7GH4zbKHvbDXpNFV-drbpNA0",
-  "authDomain": "studio-4588793295-cb701.firebaseapp.com",
-  "storageBucket": "studio-4588793295-cb701.appspot.com",
-  "measurementId": "",
+  "authDomain": "nyayagpt-426415.firebaseapp.com",
+  "storageBucket": "nyayagpt-426415.appspot.com",
+  "measurementId": "G-53091211E0",
   "messagingSenderId": "247681701203"
 };
-
